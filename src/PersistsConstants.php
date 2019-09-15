b33b6a3d4519442661cac36c796432c1bbe79ca3
Joe Macbook <joe_rushton@hotmail.co.uk>
2019-09-15 14:29:29 +0100

    wip

---
+++ src/PersistsConstants.php
@@ -0,0 +1,33 @@
+<?php
+namespace JoeyRush\PersistableConstants;
+
+trait PersistsConstants
+{
+    public static function persistConstants()
+    {
+        $reflectedClass = new \ReflectionClass(__CLASS__);
+        $constants = $reflectedClass->getConstants();
+
+        $instance = new static;
+
+        $table = \DB::table($instance->constantsTable);
+        $table->truncate();
+        $table->insert($instance->formatConstantsForDB($constants));
+    }
+
+    /**
+     * @param $constants
+     * @return array
+     */
+    public function formatConstantsForDB(array $constants): array
+    {
+        return collect($constants)->map(function ($id, $key) {
+            return [
+                'id' => $id,
+                'name' => strtolower($key),
+                'created_at' => now()->format('Y-m-d H:i:s'),
+                'updated_at' => now()->format('Y-m-d H:i:s')
+            ];
+        })->toArray();
+    }
+}
